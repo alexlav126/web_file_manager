12fc87d0be24d971cf51f2926bbfce7f9ead64d0
user <you@example.com>
2020-11-20 00:28:21 +0200

    add myComponent

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>This is MyComponent</p>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      msg: 'Hello!',
+    };
+  },
+};
+</script> 
